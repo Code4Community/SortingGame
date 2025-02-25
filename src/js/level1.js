e3452c30249e649c39c43837305c9ec576599dd0
Andrew White <drewbywhite9@gmail.com>
2025-02-24 20:13:27 -0500

    Finished basic functionality of displaying images from an array

--- src/js/level1.js
+++ src/js/level1.js
@@ -5,6 +5,7 @@ export default class Level1 extends Phaser.Scene {
     graphics;
     path;
     follower;
+    followerPath;
 
     preload() {
         // Load the background image
@@ -15,10 +16,14 @@ export default class Level1 extends Phaser.Scene {
         const greenStripedCircle = new Candy(Colors.GREEN, Shapes.CIRCLE, Patterns.STRIPED, '../assets/candy_photos/green_circle_striped.png');
         //console.log(blueStripedCircle.imagePath === '../assets/blue_circle_striped.png');
         this.load.image('background', 'assets/background.png');
-        this.load.image('follower1', blueStripedCircle.imagePath); // Load the candy image
-        this.load.image('follower2', redStripedCircleStripedCircle.imagePath);
-        this.load.image('follower3', greenStripedCircleStripedCircle.imagePath);
-        //this.load.image('follower', 'assets/follower.png'); // Optional: Load a follower sprite
+        this.load.image('follower1', 'assets/candy_photos/blue_circle_striped.png'); // Load the candy image
+        this.load.image('follower2', 'assets/candy_photos/red-circle-stripes.png');
+        this.load.image('follower3', 'assets/candy_photos/green-circle-striped.png');
+        this.load.image('conveyorDown', 'assets/conveyer_photos/ConveyerDown.png');
+        this.load.image('conveyorLeft', 'assets/conveyer_photos/Left Belt.png');
+        this.load.image('conveyorRight', 'assets/conveyer_photos/Right belt.png');
+        this.load.image('conveyorConnector', 'assets/conveyer_photos/ConnectorAll.png')
+        this.load.image('follower', 'assets/follower.png'); // Load the follower image
     }
 
     create() {
@@ -56,24 +61,34 @@ export default class Level1 extends Phaser.Scene {
         // 2 = red circle striped
         // 3 = green circle striped
         let arr1 =[
-            [1,2,3],
-            [3,3,3],
-            [3,2,1]];
-        for(i = 0; i < arr1.length; i++){
-            for(j = 0; j < arr1[i].length; j++){
-                if (arr[i][j] == 1){
-                    this.add.image(i*10 + 400, j*10 + 300, 'follower1');
-                } else if (arr[i][j] == 2){
-                    this.add.image(i*10, j*10, 'follower2');
-                } else if (arr[i][j] == 3){
-                    this.add.image(i*10, j*10, 'follower3');
+            [1,4,2,5,2],
+            [3,3,3,1,7],
+            [3,2,1,2,4],
+            [5,7,6,3,2],
+            [1,5,2,6,3]];
+        for(let i = 0; i < arr1.length; i++){
+            for(let j = 0; j < arr1[i].length; j++){
+                if (arr1[i][j] == 1){
+                    this.add.image(i*124 + 100, j*124 + 100, 'follower1');
+                } else if (arr1[i][j] == 2){
+                    this.add.image(i*124 + 100, j*124 + 100, 'follower2');
+                } else if (arr1[i][j] == 3){
+                    this.add.image(i*124 + 100, j*124 + 100, 'follower3');
+                } else if (arr1[i][j] == 4){
+                    this.add.image(i*124 + 100, j*124 + 100, 'conveyorDown');
+                } else if (arr1[i][j] == 5){
+                    this.add.image(i*124 + 100, j*124 + 100, 'conveyorLeft');
+                } else if (arr1[i][j] == 6){
+                    this.add.image(i*124 + 100, j*124 + 100, 'conveyorRight');
+                } else if (arr1[i][j] == 7){
+                    this.add.image(i*124 + 100, j*124 + 100, 'conveyorConnector');
                 }
             }
         }
             
         this.graphics = this.add.graphics();
-        this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
-        //Add follower sprite here...
+        this.follower = this.add.sprite(100, 100, 'follower'); // Create the follower sprite
+        this.followerPath = { t: 0, vec: new Phaser.Math.Vector2() };
 
         // Create the path using two separate lines
         const line1 = new Phaser.Curves.Line([100, 100, 500, 200]);
@@ -85,7 +100,7 @@ export default class Level1 extends Phaser.Scene {
 
         // Tween the follower along the path
         this.tweens.add({
-            targets: this.follower,
+            targets: this.followerPath,
             t: 1,
             ease: 'Linear',
             duration: 4000,
@@ -105,14 +120,13 @@ export default class Level1 extends Phaser.Scene {
         this.path.draw(this.graphics);
 
         // Get the position of the follower on the path
-        this.path.getPoint(this.follower.t, this.follower.vec);
+        this.path.getPoint(this.followerPath.t, this.followerPath.vec);
 
-        // Draw the follower as a red square
-        this.graphics.fillStyle(0xff0000, 1);
-        this.graphics.fillRect(this.follower.vec.x - 8, this.follower.vec.y - 8, 16, 16);
+        // Update the follower sprite's position
+        this.follower.setPosition(this.followerPath.vec.x, this.followerPath.vec.y);
     }
 }
 
 //For debugging for casey later...
 // const canvas = document.getElementById('my-custom-canvas');
-// if (canvas) {console.log("Found?");} else { console.log("Not found?"); } 
+// if (canvas) {console.log("Found?");} else { console.log("Not found?"); }
