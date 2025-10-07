b9e31fa2c15c6f5f16d6627ac878f45b03d6e706
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-10-06 20:16:25 -0400

    Create popup functionality

---
+++ src/js/popup.js
@@ -0,0 +1,5 @@
+console.log("hlol")
+document.getElementById("closePopup").onclick = function() {
+    console.log("clicked")
+    document.getElementById("popupOverlay").style.display = "none";
+};
